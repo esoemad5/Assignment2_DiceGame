02eee770c60923d88ccf8e728c8b56f158206d1c
esoemad5 <esoemad5@gmail.com>
2018-09-11 11:25:20 -0500

    Added function 'convertBoard' to take the board written as HTML script and convert it to a 2d array with the help of my sollution to 'Remove From String' from the problem solving set (feelsgoodman). The goal is to remove all the HTML tags from the string and be left with '--*--@--*- etc' which will then be put into the array character by character. Turns out, withoutString works fine with a regular string (like this message), but when the string is '<tr><td>-</td> etc' Lots more work required.

--- DiceGame.js
+++ DiceGame.js
@@ -2,8 +2,12 @@ function bestRollDice(){
 	return 4; // chosen by fair dice roll. guaranteed to be random.
 }
 
+
 function gameAdvance(dieSize, direction){
 	
+}
+function test(input){
+	console.log(input);
 }
 
 function roll_dX(dX){// roll a die with x sides
@@ -32,8 +36,55 @@ function makeGameBoard(){
 			}			
 		}
 	}
+	//test(output);
 	document.getElementById("gameBoard").innerHTML = output;
+	let a = document.getElementById("gameBoard").innerHTML;
+	test("printing a");
+	test(a);
 }
+
+function convertBoard(){
+	let board = document.getElementById("gameBoard").innerHTML;
+//	board = withoutString(board, "<tbody>");
+//	board = withoutString(board, "</tbody>");
+	//board = withoutString(board, "<tr>");
+	//board = withoutString(board, "</tr>");
+	board = withoutString(board, "<td>");
+	//board = withoutString(board, "</td>");
+	test(board);
+}
+function withoutString(base, remove){
+	let output = "";
+	for(let i = 0; i < base.length; i++){
+		if(base[i] == remove[0]){
+			if(!checkRestOfRemoveString(base, remove, i)){
+				output += base[i];
+			}
+			else{// If remove string is found, don't add it to the output string and move i forward.
+				i += remove.length;
+			}
+		}
+		else{
+			output += base[i];
+		}
+	}
+	return output;
+}
+function checkRestOfRemoveString(base, remove, positionInBaseString){
+	for(let j = 0; j < remove.length; j++){
+		if(base[positionInBaseString + j] != remove[j]){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
 
 
 function changeOutputArea(input){
